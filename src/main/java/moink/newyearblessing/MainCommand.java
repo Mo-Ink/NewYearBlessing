9161f0123809bdc4fcb9c6260409d3c74a58a1c3
Mo_Ink <2350745751@qq.com>
2022-01-27 18:38:29 +0800

    "你他妈是不是没学过代码复用啊"

--- src/main/java/moink/newyearblessing/MainCommand.java
+++ src/main/java/moink/newyearblessing/MainCommand.java
@@ -6,6 +6,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
@@ -16,9 +17,10 @@ public class MainCommand implements CommandExecutor {
 
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         Plugin plugin = NewYearBlessing.getProvidingPlugin(NewYearBlessing.class);
-        String messageFront = ChatColor.YELLOW + plugin.getConfig().getString("message-front");
+        FileConfiguration config = plugin.getConfig();
+        String messageFront = ChatColor.YELLOW + config.getString("message-front");
         if (commandSender instanceof ConsoleCommandSender) {
-            String messageConsole = plugin.getConfig().getString("message-console");
+            String messageConsole = config.getString("message-console");
             commandSender.sendMessage(ChatColor.YELLOW + messageFront + ChatColor.AQUA + messageConsole);
             return false;
         }
@@ -35,46 +37,46 @@ public class MainCommand implements CommandExecutor {
             String Message0 = strings[0];
             if (Message0.equals("add")) {
                 if (!Utils.isWithinTheTime()) {
-                    String messageTime = plugin.getConfig().getString("message-notime");
+                    String messageTime = config.getString("message-notime");
                     player.sendMessage(messageFront + ChatColor.RED + messageTime);
                     return false;
                 }
                 String Message1 = strings[1];
-                List<String> blessings = plugin.getConfig().getStringList("Blessings");
-                List<String> alreadyPlayers = plugin.getConfig().getStringList("AlreadyPlayers");
+                List<String> blessings = config.getStringList("Blessings");
+                List<String> alreadyPlayers = config.getStringList("AlreadyPlayers");
                 for (String command_ : alreadyPlayers) {
                     if (command_.equals(player.getName())) {
-                        String messageAlready = plugin.getConfig().getString("message-already");
+                        String messageAlready = config.getString("message-already");
                         player.sendMessage(messageFront + ChatColor.RED + messageAlready);
                         return false;
                     }
                 }
                 blessings.add("§l" + Message1 + " §r§7——" + player.getDisplayName());
                 alreadyPlayers.add(player.getName());
-                plugin.getConfig().set("Blessings", blessings);
-                plugin.getConfig().set("AlreadyPlayers", alreadyPlayers);
+                config.set("Blessings", blessings);
+                config.set("AlreadyPlayers", alreadyPlayers);
                 plugin.saveConfig();
-                String messageSuccess = plugin.getConfig().getString("message-success");
+                String messageSuccess = config.getString("message-success");
                 player.sendMessage(messageFront + ChatColor.GREEN + messageSuccess);
 
-                List<String> rewards = plugin.getConfig().getStringList("rewards");
+                List<String> rewards = config.getStringList("rewards");
                 int random = new Random().nextInt(rewards.size()); //随机选择
                 Bukkit.dispatchCommand(Bukkit.getConsoleSender(), rewards.get(random).replace("%player_name%", player.getName()));
                 return false;
             } else {
-                String messageError = plugin.getConfig().getString("message-error");
+                String messageError = config.getString("message-error");
                 player.sendMessage(messageFront + ChatColor.RED + messageError);
             }
         } else if (strings.length == 1) {
             String Message0 = strings[0];
             if (Message0.equals("list")) {
                 if (!Utils.isWithinTheTime()) {
-                    String messageTime = plugin.getConfig().getString("message-notime");
+                    String messageTime = config.getString("message-notime");
                     player.sendMessage(messageFront + ChatColor.RED + messageTime);
                     return false;
                 }
                 player.sendMessage(messageFront + ChatColor.GREEN + "————————————————祝福列表————————————————————");
-                List<String> blessings = plugin.getConfig().getStringList("Blessings");
+                List<String> blessings = config.getStringList("Blessings");
                 for (String command_ : blessings) {
                     player.sendMessage(messageFront + ChatColor.WHITE + command_);
                 }
@@ -82,18 +84,18 @@ public class MainCommand implements CommandExecutor {
             } else if (Message0.equals("reload")) {
                 if (player.hasPermission("nyb.commands.reload")) {
                     plugin.reloadConfig();
-                    String messageReload = plugin.getConfig().getString("message-reload");
+                    String messageReload = config.getString("message-reload");
                     player.sendMessage(messageFront + ChatColor.GREEN + messageReload);
                 } else {
-                    String messageNo = plugin.getConfig().getString("message-no");
+                    String messageNo = config.getString("message-no");
                     player.sendMessage(messageFront + ChatColor.RED + messageNo);
                 }
             } else {
-                String messageError = plugin.getConfig().getString("message-error");
+                String messageError = config.getString("message-error");
                 player.sendMessage(messageFront + ChatColor.RED + messageError);
             }
         } else {
-            String messageError = plugin.getConfig().getString("message-error");
+            String messageError = config.getString("message-error");
             player.sendMessage(messageFront + ChatColor.RED + messageError);
         }
         return false;
